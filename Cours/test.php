7a7beb767ba5286799c0b88384e038637e0af074
bngesp <bassiroungom@esp.sn>
2019-07-09 16:17:00 +0000

    Correction du TP4

--- Cours/test.php
+++ Cours/test.php
@@ -1,9 +1,18 @@
 <?php
+	
+// function __autoload($class_name)
+// {
+// 	$class_name = str_replace("\\", "/", $class_name);
+// 	var_dump($class_name);
+// 	require $class_name.".php";
+// }	
+include 'Autoload/Autoloader.php';
 
-include 'Namespaces/Personne.php';
-use Cours\Classe\User\Personne;
+use Namespaces\User\Personne;
+use Namespaces\DB\DataBase;
 
-$user = new Personne("ngom", "Bassirou");
+$user = new Personne("wade", "Massamba");
+$bd = new DataBase();
 
-var_dump($user);
+var_dump($bd);
 
